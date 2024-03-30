0922a89739151c9d0e6e0eb2acabe778dd581bb8
Diego Ivan Cundapí León <165214341+Diego-Cundapi@users.noreply.github.com>
2024-03-30 14:05:19 -0600

    volviendo a meter los archivos

---
+++ introduccion-a-php/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Answer{
+    /**
+     * Comentario de varias
+     * lineas para
+     * palabras
+     */
+    protected $clients =[]; //clientes
+    protected $insurers =[]; //aseguradoras
+}
+
+
+?>
