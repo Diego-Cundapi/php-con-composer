ab527dc00303e04de0feda0da157719040d2ef8b
Diego Ivan <Diego_cundapi@hotmail.com>
2024-03-30 13:08:55 -0600

    hice una demostracion de como se usan las asignacion y como se llevan a cabo las operaciones aritmeticas

---
+++ introduccion-a-php/asignacion-y-aritmetica.php
@@ -0,0 +1,16 @@
+<?php 
+// Asignacion
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'inglés'
+];
+
+//Aritmetica
+echo "suma 2+2: " .((int) 2 + (int) 2) . "\n";
+echo "resta 2-2: " .((int) 2 - (int) 2) . "\n";
+echo "multipicacion 2 * 2: " . 2 * 2 . "\n";
+echo "Division 2 / 2: " . 2 / 2 . "\n";
+echo "Residuo 2 % 2: " . 2 % 2 . "\n";
+echo "Exponencial 2 ** 2: " . 2 ** 2;
+?>
