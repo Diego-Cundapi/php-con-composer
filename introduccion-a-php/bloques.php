4f1d2148f7017bff16469170730fc2239b2d77ec
Diego Ivan <Diego_cundapi@hotmail.com>
2024-03-30 18:59:12 -0600

    vemos como se crean los bucles

---
+++ introduccion-a-php/bloques.php
@@ -0,0 +1,42 @@
+<?php 
+
+if () {
+    # code...
+} elseif () {
+    # code...
+} else {
+    # code...
+}
+
+switch ($login) {
+    case true:
+        # code...
+        break;
+    
+    case false:
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+//consultar, usuarios
+foreach ($datos as $id => $name) {
+    # code...
+}
+$a=1;
+while ($a <= 10) {
+    # code...
+    $a++;
+}
+
+do {
+    # code...
+} while ($a <= 10);
+
+for ($i=0; $i<10 ; $i++) {  
+    # code...
+}
+?>
