0eeb1d473ec354ae92afd95d22bd15973b26a7b7
Diego Ivan <Diego_cundapi@hotmail.com>
2024-03-30 19:42:07 -0600

    primer clase con composer

---
+++ composer/index.php
@@ -0,0 +1,6 @@
+<?php 
+require __DIR__. '/vendor/autoload.php'; //instalamos el vendor en mi archivo index para que pueda encontrar la clase 'Format'
+echo Text\Format::upperText('hola');
+
+echo upper('hola');
+?>
